2eba6ce91be82abc0b8aa8bda3c4f743f8258aca
Aswinkumarps003 <aswinkumarps2026@mca.ajce.in>
2025-04-02 21:42:11 +0530

    hihi

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.VITE_API_URL || 'https://todolist-backend-0fky.onrender.com';
+const baseURL = import.meta.env.VITE_API_URL || 'https://reactclass-backend.onrender.com';
 
 const instance = axios.create({
   baseURL,
